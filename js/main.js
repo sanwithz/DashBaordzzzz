e727ca9fc2a6768a5ea392d06165efe7a152562a
sanwithz <sanwithz@gmail.com>
2023-04-06 13:06:14 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let btnMenu = document.querySelector('.toggle-icon i');
+let menu = document.querySelector('.sidebar-wrapper');
+let closeBtn = document.querySelector('.close-menu i');
+let switcherBtn = document.querySelector('.switcher-icon i');
+let switcherContainer = document.querySelector('.switcher-container');
+let switcherClose = document.querySelector('.switcher-close i');
+
+//Function add Class
+function addClass(button, containerName, className) {
+    button.addEventListener('click', () => {
+        containerName.classList.add(className);
+    });
+}
+
+//Function remove Class
+function removeClass(button, containerName, className) {
+    button.addEventListener('click', () => {
+        containerName.classList.remove(className);
+    });
+}
+
+addClass(btnMenu, menu, 'active');
+removeClass(closeBtn, menu, 'active');
+addClass(switcherBtn, switcherContainer, 'open');
+removeClass(switcherClose, switcherContainer, 'open');
+
+//Change Color
+let colorsBtn = document.querySelectorAll('.switcher-body ul li');
+
+colorsBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        colorsBtn.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+        if (e.target.dataset.color === "#212529") {
+            document.documentElement.style.setProperty('--lightGray', e.target.dataset.color);
+            document.documentElement.style.setProperty('--whiteColor', '#2b3035');
+            document.documentElement.style.setProperty('--textColor', '#9ca2a8');
+            //Set Colors in local storage
+            let colors = [e.target.dataset.color, '#2b3035', '#9ca2a8'];
+            localStorage.setItem("colors-options", JSON.stringify(colors));
+        } else {
+            document.documentElement.style.setProperty('--lightGray', e.target.dataset.color);
+            document.documentElement.style.setProperty('--whiteColor', '#ffffff');
+            document.documentElement.style.setProperty('--textColor', '#4c5258');
+            //Set Colors in local storage
+            let colors = [e.target.dataset.color, '#ffffff', '#4c5258'];
+            localStorage.setItem("colors-options", JSON.stringify(colors));
+        }
+    });
+});
+
+//Read Colors From LocalStorage
+let colorsStorage = JSON.parse(localStorage.getItem('colors-options'));
+//Check If Local Storage is not empty
+if (colorsStorage !== null) {
+    document.documentElement.style.setProperty('--lightGray', colorsStorage[0]);
+    document.documentElement.style.setProperty('--whiteColor', colorsStorage[1]);
+    document.documentElement.style.setProperty('--textColor', colorsStorage[2]);
+}
